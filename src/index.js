75f995b60a41f470d4ca6879bf2b41835b317470
Vaibhav Harsoda <vaibhavharsoda77@gmail.com>
2023-05-23 14:57:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import axios from 'axios';
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
-
+console.log(process.env.REACT_APP_API_URL);
 root.render(
   <React.StrictMode>
     <App />
