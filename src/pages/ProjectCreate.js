7e30fda67159fb7fb8b0bfd15bc51e30eeb3169f
Vaibhav Harsoda <vaibhavharsoda77@gmail.com>
2023-05-29 12:18:08 +0530

    Update ProjectCreate.js

--- src/pages/ProjectCreate.js
+++ src/pages/ProjectCreate.js
@@ -1,10 +1,10 @@
-import React,{ useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from "react-router-dom"
 import Swal from 'sweetalert2'
 import axios from 'axios'
 import Layout from "../components/Layout"
- 
- 
+
+
 function ProjecCreate() {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('')
@@ -12,50 +12,50 @@ function ProjecCreate() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        if(localStorage.getItem('token') == null) {
+        if (localStorage.getItem('token') == null) {
             navigate("/");
         }
     }, [])
 
     const axiosInstance = axios.create({
-      baseURL: 'https://mock-api.binaryboxtuts.com/',
+        baseURL: 'https://mock-api.binaryboxtuts.com/',
     });
-    
+
     const handleSave = () => {
         setIsSaving(true);
         axiosInstance.post('/api/projects', {
             name: name,
             description: description
-          })
-          .then(function (response) {
-            Swal.fire({
-                icon: 'success',
-                title: 'Project saved successfully!',
-                showConfirmButton: false,
-                timer: 1500
+        })
+            .then(function (response) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Project saved successfully!',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                setIsSaving(false);
+                setName('')
+                setDescription('')
             })
-            setIsSaving(false);
-            setName('')
-            setDescription('')
-          })
-          .catch(function (error) {
-            Swal.fire({
-                icon: 'error',
-                title: 'An Error Occured!',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            setIsSaving(false)
-          });
+            .catch(function (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'An Error Occured!',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                setIsSaving(false)
+            });
     }
-  
+
     return (
         <Layout>
             <div className="container">
                 <h2 className="text-center mt-5 mb-3">Create New Project</h2>
                 <div className="card">
                     <div className="card-header">
-                        <Link 
+                        <Link
                             className="btn btn-outline-info float-right"
                             to="/dashboard">View All Projects
                         </Link>
@@ -64,27 +64,27 @@ function ProjecCreate() {
                         <form>
                             <div className="form-group">
                                 <label htmlFor="name">Name</label>
-                                <input 
-                                    onChange={(event)=>{setName(event.target.value)}}
+                                <input
+                                    onChange={(event) => { setName(event.target.value) }}
                                     value={name}
                                     type="text"
                                     className="form-control"
                                     id="name"
-                                    name="name"/>
+                                    name="name" />
                             </div>
                             <div className="form-group">
                                 <label htmlFor="description">Description</label>
-                                <textarea 
+                                <textarea
                                     value={description}
-                                    onChange={(event)=>{setDescription(event.target.value)}}
+                                    onChange={(event) => { setDescription(event.target.value) }}
                                     className="form-control"
                                     id="description"
                                     rows="3"
                                     name="description"></textarea>
                             </div>
-                            <button 
+                            <button
                                 disabled={isSaving}
-                                onClick={handleSave} 
+                                onClick={handleSave}
                                 type="button"
                                 className="btn btn-outline-primary mt-3">
                                 Save Project
@@ -96,5 +96,5 @@ function ProjecCreate() {
         </Layout>
     );
 }
-  
+
 export default ProjecCreate;
