2279dbd15135fc513977d651a300624a1b962663
Vaibhav Harsoda <vaibhavharsoda77@gmail.com>
2023-05-29 12:18:02 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react'
-import { Link , useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import Swal from 'sweetalert2'
 import axios from 'axios'
 import Layout from "../components/Layout"
 
-function Login() {  
+function Login() {
     const navigate = useNavigate();
     const [email, setEmail] = useState('admin');
     const [password, setPassword] = useState('11')
     const [isSaving, setIsSaving] = useState(false);
 
-    useEffect(()=>{
-        if(localStorage.getItem('user') && localStorage.getItem('user') != null){
+    useEffect(() => {
+        if (localStorage.getItem('user') && localStorage.getItem('user') != null) {
             navigate("/dashboard");
         }
-    },[])
+    }, [])
 
     const instance = axios.create({
         baseURL: 'https://dummyjson.com/',
-      });
+    });
 
     const handleSave = () => {
         /* kminchelle */
@@ -26,34 +26,34 @@ function Login() {
         setIsSaving(true);
         var em = email === 'admin' ? 'kminchelle' : email;
         var pas = password === '11' ? '0lelplR' : password;
-        
+
         instance.post('/auth/login', {
             username: em,
             password: pas
-          })
-          .then(function (response) {
-            localStorage.setItem("user", JSON.stringify(response.data));
-            localStorage.setItem("token", response.data.token);
-            Swal.fire({
-                icon: 'success',
-                title: 'Login successfully!',
-                showConfirmButton: false,
-                timer: 1500
+        })
+            .then(function (response) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+                localStorage.setItem("token", response.data.token);
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Login successfully!',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                navigate("/dashboard");
+                setIsSaving(false);
+                setEmail('')
+                setPassword('')
             })
-            navigate("/dashboard");
-            setIsSaving(false);
-            setEmail('')
-            setPassword('')
-          })
-          .catch(function (error) {
-            Swal.fire({
-                icon: 'error',
-                title: 'An Error Occured!',
-                showConfirmButton: false,
-                timer: 1500
-            })
-            setIsSaving(false)
-          });
+            .catch(function (error) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'An Error Occured!',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+                setIsSaving(false)
+            });
     }
 
     return (
@@ -66,36 +66,36 @@ function Login() {
                                 <h5 className="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
                                 <form>
                                     <div className="form-floating mb-3">
-                                        <input 
-                                        value={email}
-                                        onChange={(event)=>{setEmail(event.target.value)}}
-                                        type="email"
-                                        className="form-control"
-                                        id="floatingInput" 
-                                        placeholder="name@example.com" 
-                                        value="admin"
-                                          />
+                                        <input
+                                            value={email}
+                                            onChange={(event) => { setEmail(event.target.value) }}
+                                            type="email"
+                                            className="form-control"
+                                            id="floatingInput"
+                                            placeholder="name@example.com"
+                                            value="admin"
+                                        />
                                         <label htmlFor="floatingInput">Email address</label>
                                     </div>
                                     <div className="form-floating mb-3">
-                                        <input 
-                                        value={password}
-                                        onChange={(event)=>{setPassword(event.target.value)}}
-                                        type="password"
-                                        className="form-control"
-                                        id="floatingPassword"
-                                        placeholder="Password" 
-                                        value="11"
+                                        <input
+                                            value={password}
+                                            onChange={(event) => { setPassword(event.target.value) }}
+                                            type="password"
+                                            className="form-control"
+                                            id="floatingPassword"
+                                            placeholder="Password"
+                                            value="11"
                                         />
                                         <label htmlFor="floatingPassword">Password</label>
                                     </div>
 
                                     <div className="d-grid">
-                                        <button 
-                                        disabled={isSaving}
-                                        onClick={handleSave} 
-                                        type="submit"
-                                        className="btn btn-primary btn-login text-uppercase fw-bold" >
+                                        <button
+                                            disabled={isSaving}
+                                            onClick={handleSave}
+                                            type="submit"
+                                            className="btn btn-primary btn-login text-uppercase fw-bold" >
                                             Sign in
                                         </button>
                                     </div>
