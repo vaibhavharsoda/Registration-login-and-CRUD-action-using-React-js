fd72d56491b732b352a401afba1b3c53cfc4fb31
Vaibhav Harsoda <vaibhavharsoda77@gmail.com>
2023-05-29 12:18:11 +0530

    Update ProjectShow.js

--- src/pages/ProjectShow.js
+++ src/pages/ProjectShow.js
@@ -1,39 +1,39 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams, useNavigate } from "react-router-dom";
 import axios from 'axios';
 import Layout from "../components/Layout"
- 
-  
+
+
 function ProjectShow() {
     const navigate = useNavigate();
     const [id, setId] = useState(useParams().id)
-    const [project, setProject] = useState({name:'', description:''})
-    
+    const [project, setProject] = useState({ name: '', description: '' })
+
     const axiosInstance = axios.create({
         baseURL: 'https://mock-api.binaryboxtuts.com/',
-      });
+    });
 
     useEffect(() => {
-        if(localStorage.getItem('token') == null) {
+        if (localStorage.getItem('token') == null) {
             navigate("/");
         }
-        
+
         axiosInstance.get(`/api/projects/${id}`)
-        .then(function (response) {
-          setProject(response.data)
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
+            .then(function (response) {
+                setProject(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }, [])
-  
+
     return (
         <Layout>
-           <div className="container">
-            <h2 className="text-center mt-5 mb-3">Show Project</h2>
+            <div className="container">
+                <h2 className="text-center mt-5 mb-3">Show Project</h2>
                 <div className="card">
                     <div className="card-header">
-                        <Link 
+                        <Link
                             className="btn btn-outline-info float-right"
                             to="/dashboard"> View All Projects
                         </Link>
@@ -49,5 +49,5 @@ function ProjectShow() {
         </Layout>
     );
 }
-  
+
 export default ProjectShow;
